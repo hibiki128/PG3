a7d7aa27efcb09f37d25060ce81f5135df60b19f
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2024-09-30 11:59:11 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
 #include"stdio.h"
 
-template<typename Type>
-Type Min(Type a, Type b) {
-	if (a > b) {
-		return b;
+int money(int hour, int normal, int recursive) {
+
+	normal += 1072;
+	recursive = (hour == 1) ? 100 : recursive * 2 - 50;
+
+	if (hour <= 24) {
+		return printf("%d時間働いて、一般的 : %d円,再帰的 : %d円でした\n",hour,normal,recursive), money(hour + 1, normal, recursive);
 	}
-	else {
-		return a;
-	}
+	return 0;
 }
 
-template<>
-char Min(char a, char b) {
-	return printf("数字以外は代入できません\n");
-}
+int hour = 1;
+int normal = 0;
+int recursive = 0;
 
 int main() {
 
-	printf("%d\n", Min<int>(5, 4));
-	printf("%f\n", Min<float>(5.31111f, 4.1666f));
-	printf("%lf\n", Min<double>(2.333333, 5.111111));
-	Min<char>(5, 6);
+	money(hour, normal, recursive);
 
 	return 0;
 }
